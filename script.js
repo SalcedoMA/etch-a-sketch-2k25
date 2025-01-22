5e541bcc166f523a5798bae2c58bf72944959c08
salcedoma <salcedoma.github@gmail.com>
2025-01-21 23:02:14 -0300

    Add prompt to ask for number of rows for the grid
    Use inputed number of rows to set height and width of each grid square.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const etch = document.querySelector('.etch');
+const rows = parseInt(prompt("What number of rows?"));
 
-for (i=0; i<72; i++) {
+for (i=0; i<(rows*rows); i++) {
     const grid = document.createElement('div');
     grid.classList = 'grid';
+    grid.style.height = `calc(100%/${rows}`;
+    grid.style.width = `calc(100%/${rows}`;
     etch.appendChild(grid);
 }
+
+const grid = document.querySelectorAll('.grid');
