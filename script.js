37edd45c014e73b8c55f9a5692ee803aaee1cd33
salcedoma <salcedoma.github@gmail.com>
2025-01-22 15:49:05 -0300

    Move for loop to reGrid function, to be activated on click event

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const etch = document.querySelector('.etch');
-const rows = parseInt(prompt("What number of rows?"));
+const redraw = document.getElementById('regrid');
 
-for (i=0; i<(rows*rows); i++) {
-    const grid = document.createElement('div');
-    grid.classList = 'grid';
-    grid.style.height = `calc(100%/${rows}`;
-    grid.style.width = `calc(100%/${rows}`;
-    etch.appendChild(grid);
+function reGrid() {
+    const rows = parseInt(prompt("What number of rows?"));
+    for (i=0; i<(rows*rows); i++) {
+        const grid = document.createElement('div');
+        grid.classList = 'grid';
+        grid.style.height = `calc(100%/${rows}`;
+        grid.style.width = `calc(100%/${rows}`;
+        etch.appendChild(grid);
+        grid.addEventListener('mouseover', event => {
+            grid.style.backgroundColor = '#FFF';
+        })
+    }
 }
 
+redraw.addEventListener('click', function() {
+    reGrid();
+    redraw.innerText = "Redraw";
+    redraw.style = "font-size: 1.3rem; padding: 0.5em 1.5em;";
+    etch.style = "display: flex;";
+
+});
 const grid = document.querySelectorAll('.grid');
